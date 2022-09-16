2fb2b2cbd351a69e23cb22d29ac49dfa9eea036c
Fulin <944799613@qq.com>
2022-09-15 20:23:16 -0500

    update

---
+++ Lab3-Intro-to-APIs/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Lab3_Intro_to_APIs.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> option) : base(option)
+        { }
+        public DbSet<Lab3_Intro_to_APIs.Models.Route> Routes { get; set; }
+        public DbSet<Lab3_Intro_to_APIs.Models.ScheduledStop> ScheduledStops { get; set; }
+        public DbSet<Lab3_Intro_to_APIs.Models.Stop> Stops { get; set; }
+
+    }
+}
